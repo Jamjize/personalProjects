a6eec52c2ff730c67b663cfbce983c4ed8b7da5f
Leo Pucheta <jamjizelol@gmail.com>
2024-01-14 00:07:00 -0300

    D simple_calc

--- python/reflex/simple_calc/components/buttons.py
+++
@@ -1,9 +0,0 @@
-import reflex as rx
-
-def button_row(symbols: list[str]) -> rx.component:
-    return rx.hstack(
-        rx.button(symbols[0]),
-        rx.button(symbols[1]),
-        rx.button(symbols[2]),
-        rx.button(symbols[3])
-    )
